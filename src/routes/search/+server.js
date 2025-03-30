3c3e4a452aafcd2340524e42e43a84d74f80bfd0
Jáchym Janoušek <janousekjachym@gmail.com>
2025-03-30 16:39:41 +0200

    implement debounce function, add search route with external API integration, and enhance app styling for scientific presentation

---
+++ src/routes/search/+server.js
@@ -0,0 +1,23 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET({ url }) {
+  const search = url.searchParams.get("q");
+  if (!search) {
+    return json([]);
+  }
+
+  const searchUrl = "http://nominatim.openstreetmap.org/search";
+  const requestUrl = `${searchUrl}?countrycodes=CZ&format=json&q=${search}`;
+  console.log(requestUrl);
+  const response = await fetch(requestUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+
+  return json(data);
+}
